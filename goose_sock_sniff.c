a3693763eaf870fe001d78c52a876a43f7a50afd
MatthewBoeding <61028228+MatthewBoeding@users.noreply.github.com>
2022-07-12 15:18:19 -0500

    Update goose_sock_sniff.c

--- goose_sock_sniff.c
+++ goose_sock_sniff.c
@@ -21,6 +21,12 @@ int main(int argc, uint8_t ** argv[])
 	int num_bytes, sock_sniff;
 	sock_sniff = socket(AF_PACKET, SOCK_RAW, htons(0x88b8));
 	setsockopt(sock_sniff, SOL_PACKET, SO_BINDTODEVICE, opt, IFNAMSIZ-1);
+	struct packet_mreq mreq;
+	memset(&mreq,0,sizeof(mreq));
+	mreq.mr_ifindex = ifr.ifr_ifindex;
+	mreq.mr_type = PACKET_MR_PROMISC;
+	mreq.mr_alen = 6;
+	setsockopt(fd,SOL_PACKET,PACKET_ADD_MEMBERSHIP,(void*)&mreq,(socklen_t)sizeof(mreq));
 
 	while(1)
 	{
