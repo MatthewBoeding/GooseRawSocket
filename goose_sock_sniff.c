e2be972637d1878402f0f5f6b9f0545a49feeb4b
MatthewBoeding <61028228+MatthewBoeding@users.noreply.github.com>
2022-07-12 15:54:46 -0500

    Sniffing Resolved
    Currently only printing endpoint. Will work out logging for later usage.

--- goose_sock_sniff.c
+++ goose_sock_sniff.c
@@ -13,26 +13,58 @@
 
 #define BUFF_SIZE  1500
 
+void getEthtype(char * message_data)
+{
+	struct ether_header *eth = (struct ether_header *) message_data;
+	uint16_t type = eth->ether_type;
+	//uint16_t temp = type;
+	type = (type << 8) | (type >> 8);
+	if(type == 0x88b8)
+	{
+		printf("Goose Received. Endpoint:");
+		char * helper = (char *)&eth->ether_dhost;
+		for(int i = 0; i < 6; i++)
+		{
+		printf("%02x",helper[i]);
+		}
+		printf("\n");
+		fflush(stdout);
+	}
+
+}
+
 int main(int argc, uint8_t ** argv[])
 {
-	char opt[IFNAMSIZ] = "eth0";
-	char message_data[BUFF_SIZE];
-	int on = 1;
-	int num_bytes, sock_sniff;
-	sock_sniff = socket(AF_PACKET, SOCK_RAW, htons(0x88b8));
-	setsockopt(sock_sniff, SOL_PACKET, SO_BINDTODEVICE, opt, IFNAMSIZ-1);
 	struct packet_mreq mreq;
 	memset(&mreq,0,sizeof(mreq));
-	mreq.mr_ifindex = ifr.ifr_ifindex;
+	char * opt= "eth0";
+	char message_data[BUFF_SIZE];
+	int on = 1;
+	int num_bytes, sock_sniff, val;
+	unsigned int len;
+	struct ifreq iface_mac;
+	val = 1;
+	len = sizeof(val);
+	sock_sniff = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+	setsockopt(sock_sniff, SOL_PACKET, PACKET_AUXDATA, &on, sizeof(on));
+	//setsockopt(sock_sniff, SOL_SOCKET, SO_BINDTODEVICE, "eth0", 4) >= 0 ? :	perror("binding failed");
+
+
+	memset(&iface_mac, 0, sizeof(struct ifreq));
+	strncpy(iface_mac.ifr_name, "eth0", IFNAMSIZ-1);
+	iface_mac.ifr_ifindex = if_nametoindex(opt);
+	mreq.mr_ifindex = iface_mac.ifr_ifindex;
 	mreq.mr_type = PACKET_MR_PROMISC;
 	mreq.mr_alen = 6;
-	setsockopt(fd,SOL_PACKET,PACKET_ADD_MEMBERSHIP,(void*)&mreq,(socklen_t)sizeof(mreq));
 
+	setsockopt(sock_sniff, SOL_PACKET,PACKET_ADD_MEMBERSHIP, (void*)&mreq, (socklen_t)sizeof(mreq));
+	ioctl(sock_sniff, SIOCGIFHWADDR, &iface_mac) >= 0 ? : perror("SIOCGIFHWADDR");
+	ioctl(sock_sniff, SIOCGIFINDEX, &iface_mac) >= 0 ? : perror("SIOCGIFINDEX");
+	printf("Attempting to sniff");
 	while(1)
 	{
-		printf("Attempting to sniff");
-		num_bytes = recvfrom(sock_sniff, message_data, BUFF_SIZE-1, 0, NULL, NULL);
-		printf("Goose Packet Received");
+		num_bytes = recv(sock_sniff, message_data, BUFF_SIZE-1, 0);
+		getEthtype(message_data);
 	}
 	return 0;
 }
